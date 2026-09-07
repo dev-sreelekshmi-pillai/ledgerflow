4d51d6fb59d4d0f94243eb93907f07da9de22b55
dev-sreelekshmi-pillai <sree0008lakshmi@gmail.com>
2026-09-07 12:12:31 +0530

    chore: initial Angular 20 setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('lenderFlowApp');
+  protected readonly title = signal('ledgerFlowApp');
 }
